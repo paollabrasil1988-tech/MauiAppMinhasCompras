0a88e4a8bc0fc60a5bc162ea099ceebf305da0fc
PAOLLA BRASIL <paollabrasil1988@gmail.com>
2026-09-04 10:08:49 -0300

    Alteração na interface XAML, alteração no Classe Model,  alteração no código C# da View

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -5,7 +5,7 @@ namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
-        readonly SQLiteAsyncConnection _conn;
+        readonly SQLiteAsyncConnection? _conn;
         public SQLiteDatabaseHelper(string path)
         {
             _conn = new SQLiteAsyncConnection(path);
@@ -13,29 +13,29 @@ namespace MauiAppMinhasCompras.Helpers
         }
         public Task <int> Insert (Produto p) 
         {
-            return _conn.InsertAsync(p);
+            return _conn!.InsertAsync(p);
         }
         public Task<List<Produto>> Update (Produto p) 
         {
             string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
-            return _conn.QueryAsync<Produto>(
+            return _conn!.QueryAsync<Produto>(
                 sql, p.Descricao, p.Quantidade, p.Preco, p.Id
                 );
 
         }
         public Task<int> Delete(int id) 
         {
-            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+            return _conn!.Table<Produto>().DeleteAsync(i => i.Id == id);
         
         }
         public Task<List<Produto>> GetAll() 
         { 
-            return _conn.Table<Produto>().ToListAsync();
+            return _conn!.Table<Produto>().ToListAsync();
         }
         public Task<List<Produto>> Search (string q) 
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" +q+ "%'";
-            return _conn.QueryAsync<Produto>(sql);
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" +q+ "%'";
+            return _conn!.QueryAsync<Produto>(sql);
         }
 
 }
